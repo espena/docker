0fc83437634a202bfd7bfc8df0324ae7bbf9d08b
Espen Andersen <post@espenandersen.no>
2017-09-13 20:41:33 +0200

    Calendar server

---
+++ davical/davical_config.php
@@ -0,0 +1,6 @@
+<?php
+  $c->domain_name = 'VHOST_NAME';
+  $c->sysabbr     = 'DAViCal';
+  $c->admin_email = 'VHOST_ADMIN';
+  $c->system_name = 'DAVICAL_SYSNAME';
+  $c->pg_connect[] = 'dbname=davical port=5432 user=davical_app';
