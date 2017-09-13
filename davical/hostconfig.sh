6191637cba98dba7906bc1228a4dca4163f11684
Espen Andersen <post@espenandersen.no>
2017-09-13 21:57:19 +0200

    Fix errors in hostconfig script.

--- davical/hostconfig.sh
+++ davical/hostconfig.sh
@@ -3,12 +3,12 @@ DAVICAL_SYSNAME='${DAVICAL_SYSNAME:-DaviCal Server}'
 VHOST_NAME='${VHOST_NAME:-localhost}'
 VHOST_ADMIN='${VHOST_ADMIN:-webmaster@localhost}'
 cd /etc/nginx/sites-available
-sed -i "s/VHOST_NAME/"$VHOST_NAME"/g default"
+sed -i "s/VHOST_NAME/"$VHOST_NAME"/g" default
 mv default $VHOST_NAME
 ln -s /etc/nginx/sites-available/$VHOST_NAME /etc/nginx/sites-enabled/
 rm /etc/nginx/sites-enabled/default
 cd /etc/davical
-sed -i "s/VHOST_ADMIN/"$VHOST_ADMIN"/g config.php"
-sed -i "s/VHOST_NAME/"$VHOST_NAME"/g config.php"
-sed -i "s/DAVICAL_SYSNAME/"$DAVICAL_SYSNAME"/g config.php"
+sed -i "s/VHOST_ADMIN/"$VHOST_ADMIN"/g" config.php
+sed -i "s/VHOST_NAME/"$VHOST_NAME"/g" config.php
+sed -i "s/DAVICAL_SYSNAME/"$DAVICAL_SYSNAME"/g" config.php
 service postgresql start && service php7.0-fpm start && service nginx start
